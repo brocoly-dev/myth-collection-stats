b0d1bcf2c173f96719ddc3fbb85d2cd3a625d46b
brocoly-dev <rivasarmando271084@icloud.com>
2025-06-05 09:13:15 -0600

    Refactored code to add spotless

---
+++ src/main/java/com/mesofi/myth/collection/stats/Application.java
@@ -0,0 +1,15 @@
+package com.mesofi.myth.collection.stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application class for the Myth Collection Stats service. This class serves as
+ * the entry point for the application and configures Spring Boot autoconfiguration.
+ */
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
